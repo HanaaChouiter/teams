2b9b62f82f4b0259bd3b113e2ba40fb1dc32bb0b
HanaaChouiter <hanaa7chouiter@gmail.com>
2021-10-27 09:47:31 +0200

    modif css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-
 import teams from './teams.json'
 import TeamInfo from './components/TeamInfo'
 
 class App extends React.Component {
   render() {
     let team = teams.map(team=>(
-      <TeamInfo name={team.shortName} />
+      <TeamInfo image={team.crestUrl} name={team.shortName} venue={team.venue} address={team.address} email={team.email} phone={team.phone} founded={team.founded}/>
     ))
     return (
       <div className="container">
--- src/components/TeamInfo.js
+++ src/components/TeamInfo.js
@@ -3,13 +3,11 @@ import React from 'react'
 
 class TeamInfo extends React.Component {
   render() {
-   
-
-    return (
+   return (
       <div className="col-3 my-3">
         <div className="p-4 border border-2 ">
-            <img src={this.props.crestUrl} className="img-fluid mb-3" alt=""/>
-            <h2 className="text-primary text-center mb-3">{this.props.shortName}</h2>
+            <img src={this.props.image} className="img-fluid mb-3" alt=""/>
+            <h2 className="text-primary text-center mb-3">{this.props.name}</h2>
             <p className="ms-1">{this.props.venue}</p>
             <p className="ms-1">{this.props.address}</p>
             <p className="text-info ms-1">{this.props.email}</p>
